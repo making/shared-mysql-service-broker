746ec53b8f4c4007ed3ecc0de44f59bf831febf3
Toshiaki Maki <tmaki@pivotal.io>
2018-02-10 03:48:16 +0900

    Fix test

--- src/test/java/am/ik/servicebroker/mysql/servicebroker/CatalogControllerTest.java
+++ src/test/java/am/ik/servicebroker/mysql/servicebroker/CatalogControllerTest.java
@@ -3,6 +3,7 @@ package am.ik.servicebroker.mysql.servicebroker;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpHeaders;
@@ -13,39 +14,47 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CatalogControllerTest {
-    @LocalServerPort
-    int port;
-    WebTestClient webClient;
+	@LocalServerPort
+	int port;
+	WebTestClient webClient;
 
-    @Before
-    public void setup() {
-        this.webClient = WebTestClient.bindToServer()
-                .baseUrl("http://localhost:" + port)
-                .build();
-    }
+	@Before
+	public void setup() {
+		this.webClient = WebTestClient.bindToServer() //
+				.baseUrl("http://localhost:" + port) //
+				.build();
+	}
 
-    @Test
-    public void catalogUnauthorized() {
-        this.webClient.get().uri("/v2/catalog")
-                .exchange()
-                .expectStatus().isEqualTo(HttpStatus.UNAUTHORIZED);
-    }
+	@Test
+	public void catalogUnauthorized() {
+		this.webClient.get().uri("/v2/catalog") //
+				.exchange() //
+				.expectStatus().isEqualTo(HttpStatus.UNAUTHORIZED);
+	}
 
-    @Test
-    public void catalogWrongUser() {
-        this.webClient.get().uri("/v2/catalog")
-                .header(HttpHeaders.AUTHORIZATION, "Basic Zm9vOmJhcg==")
-                .exchange()
-                .expectStatus().isEqualTo(HttpStatus.UNAUTHORIZED);
-    }
-    
-    @Test
-    public void catalog() {
-        this.webClient.get().uri("/v2/catalog")
-                .header(HttpHeaders.AUTHORIZATION, "Basic dGVzdDpiYXI=")
-                .exchange()
-                .expectStatus().isEqualTo(HttpStatus.OK)
-                .expectBody()
-                .jsonPath("$.services[0].id", "445d9bca-ba54-11e7-8f28-89aab798d359");
-    }
+	@Test
+	public void catalogWrongUser() {
+		this.webClient.get().uri("/v2/catalog") //
+				.header(HttpHeaders.AUTHORIZATION, "Basic Zm9vOmJhcg==") //
+				.exchange() //
+				.expectStatus().isEqualTo(HttpStatus.UNAUTHORIZED);
+	}
+
+	@Test
+	public void catalog() {
+		this.webClient.get().uri("/v2/catalog") //
+				.header(HttpHeaders.AUTHORIZATION, "Basic dGVzdDpiYXI=") //
+				.exchange() //
+				.expectStatus().isEqualTo(HttpStatus.OK) //
+				.expectBody() //
+				.jsonPath("$.services[0].id")
+				.isEqualTo("445d9bca-ba54-11e7-8f28-89aab798d359") //
+				.jsonPath("$.services[0].name").isEqualTo("shared-mysql") //
+				.jsonPath("$.services[0].bindable").isEqualTo(true) //
+				.jsonPath("$.services[0].tags[0]").isEqualTo("mysql") //
+				.jsonPath("$.services[0].metadata.displayName").isEqualTo("Shared MySQL") //
+				.jsonPath("$.services[0].plans[0].id")
+				.isEqualTo("517cf274-ba54-11e7-8f28-89aab798d359") //
+				.jsonPath("$.services[0].plans[0].name").isEqualTo("shared");
+	}
 }
