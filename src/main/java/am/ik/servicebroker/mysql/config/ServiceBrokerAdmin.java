671f8903f9c18b5ee463a29935d07d5406a5e123
Toshiaki Maki <tmaki@pivotal.io>
2018-06-14 13:41:30 +0900

    Bump to Spring Boot 2.0.2

--- src/main/java/am/ik/servicebroker/mysql/config/ServiceBrokerAdmin.java
+++ src/main/java/am/ik/servicebroker/mysql/config/ServiceBrokerAdmin.java
@@ -3,6 +3,7 @@ package am.ik.servicebroker.mysql.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @ConfigurationProperties(prefix = "service-broker.admin")
@@ -24,14 +25,13 @@ public class ServiceBrokerAdmin {
 	}
 
 	public void setPassword(String password) {
-		this.password = password;
+		this.password = new Pbkdf2PasswordEncoder().encode(password);
 	}
 
 	public UserDetails asUserDetails() {
-		return User.withDefaultPasswordEncoder() //
-                .username(this.username) //
-				.password(this.password) //
-                .roles("ADMIN") //
-                .build();
+		return User.withUsername(this.username) //
+				.password("{pbkdf2}" + this.password) //
+				.roles("ADMIN") //
+				.build();
 	}
 }
